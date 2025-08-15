bbf95ca966098deb95898892339602f8e19e9eff
Samr Mouna <samr.mouna@gmail.com>
2025-08-15 00:07:13 -0400

    Added gitignore and setting up react routers

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { router, createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <NavBar />
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
